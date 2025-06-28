658c695ee8654138ce6ced73dab8632028527323
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-06-29 00:55:30 +0100

    🚀 Initialisation des états des capteurs dans DataSimulator
    📦 Contenu :
    - Création de la méthode _init_sensors pour initialiser les capteurs simulés
    - Support des capteurs : température, humidité, rpm, vibration, énergie, uptime, status
    - Utilisation de valeurs par défaut si absentes dans config.yaml
    - Ajout de paramètres : current, min, max, variation pour chaque capteur
    - Ajout de la gestion du temps de fonctionnement (uptime) et de l'état machine (status)

--- data_simulator.py
+++ data_simulator.py
@@ -24,5 +24,50 @@ class DataSimulator:
         self.sensor_states = self._init_sensors()
         
         logger.info("Simulateur de données initialisé")
-
     
+    def _init_sensors(self) -> Dict[str, Any]:
+        """Initialiser les états des capteurs"""
+        return {
+            'temperature': {
+                'current': self.sensors_config.get('temperature', {}).get('initial', 25.0),
+                'min': self.sensors_config.get('temperature', {}).get('min', 15.0),
+                'max': self.sensors_config.get('temperature', {}).get('max', 85.0),
+                'variation': self.sensors_config.get('temperature', {}).get('variation', 2.0)
+            },
+            'humidity': {
+                'current': self.sensors_config.get('humidity', {}).get('initial', 50.0),
+                'min': self.sensors_config.get('humidity', {}).get('min', 30.0),
+                'max': self.sensors_config.get('humidity', {}).get('max', 90.0),
+                'variation': self.sensors_config.get('humidity', {}).get('variation', 5.0)
+            },
+            'rpm': {
+                'current': self.sensors_config.get('rpm', {}).get('initial', 1450),
+                'min': self.sensors_config.get('rpm', {}).get('min', 0),
+                'max': self.sensors_config.get('rpm', {}).get('max', 3000),
+                'variation': self.sensors_config.get('rpm', {}).get('variation', 50)
+            },
+            'vibration': {
+                'current': self.sensors_config.get('vibration', {}).get('initial', 1.0),
+                'min': self.sensors_config.get('vibration', {}).get('min', 0.5),
+                'max': self.sensors_config.get('vibration', {}).get('max', 5.0),
+                'variation': self.sensors_config.get('vibration', {}).get('variation', 0.2)
+            },
+            'energy': {
+                'current': self.sensors_config.get('energy', {}).get('initial', 2.5),
+                'min': self.sensors_config.get('energy', {}).get('min', 0.5),
+                'max': self.sensors_config.get('energy', {}).get('max', 15.0),
+                'variation': self.sensors_config.get('energy', {}).get('variation', 0.5)
+            },
+            'uptime': {
+                'start_time': time.time(),
+                'total': 0
+            },
+            'status': {
+                'current': 'ON',
+                'states': ['ON', 'OFF', 'ERREUR']
+            }
+        }
+    
+  
+    
+ 
